93f5d501ea77bdaa1c26d4e546d32080a44fc93b
IsaakJG <isaak.j.garside@gmail.com>
2022-04-21 10:40:06 +0100

    simple error handling for invalid article id's

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -5,13 +5,30 @@ import { Card } from "react-bootstrap";
 
 const SingleArticle = () => {
   const [article, setArticle] = useState([]);
+  const [err, setErr] = useState(null);
   const { article_id } = useParams();
 
   useEffect(() => {
-    singleArticle(article_id).then((articleFromApi) => {
-      setArticle(articleFromApi);
-    });
+    singleArticle(article_id)
+      .then((articleFromApi) => {
+        setArticle(articleFromApi);
+        setErr(null);
+      })
+      .catch(() => {
+        setErr("Article not found :(");
+      });
   }, [article_id]);
+
+  if (err)
+    return (
+      <div>
+        <Card>
+          <Card.Body>
+            <Card.Title>Article not found 😢</Card.Title>
+          </Card.Body>
+        </Card>
+      </div>
+    );
 
   return (
     <div>
